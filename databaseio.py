e0143d8126adee4dcce358aa4414cce183a43eb5
petute <daniel.petutschnigg@gmail.com>
2021-01-05 16:16:41 +0100

    Add function create_db Added create_db function in order to set up a database. Also added test.

--- databaseio.py
+++ databaseio.py
@@ -2,3 +2,21 @@ import sqlite3
 import crypto
 
 # Class to handle all sqlite database interaction.
+
+
+# Function to create database and to create the table.
+
+
+def create_db(database):
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+
+    c.execute(
+        """CREATE TABLE entries
+                    (name text, username text, 
+                    password text, description text)"""
+    )
+
+    conn.commit()
+    conn.close()
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import databaseio, crypto
 
 database = "test.db"
+# databaseio.createDb(database)
